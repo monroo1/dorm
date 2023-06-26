cf2ca7a2e08a9a1f083d21fb93df3b181fb65dda
monroo <andrey.monroo@gmail.com>
2023-06-26 20:32:13 +0300

    install react and sass loader for webpack

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./Counter.scss";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="counter">
+      <h1>{count}</h1>
+      <button onClick={increment}>increment</button>
+    </div>
+  );
+};
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { someFn } from "./test";
-
-someFn(1);
-
-document.body.innerHTML = `<div>hello wordsad111!</div>`;
