a9c09a933a14ef6ff080648f024ecd95a25c3869
monroo <andrey.monroo@gmail.com>
2023-06-27 16:27:14 +0300

    themes, hook, var scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import { Link } from "react-router-dom"
 import { Suspense } from "react"
+import { useTheme } from "./theme/useTheme"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
 
-import "./index.scss"
+import "./styles/index.scss"
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
