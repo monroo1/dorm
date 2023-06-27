6dfc7cdec62ac2eb29c73b16c69ecfd44a9ddf1f
monroo <andrey.monroo@gmail.com>
2023-06-27 13:32:42 +0300

    react-router-dom, suspense, lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Counter } from "./components/Counter"
+import { Route, Routes } from "react-router-dom"
+import { Link } from "react-router-dom"
+import { Suspense } from "react"
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
+import { MainPageAsync } from "./pages/MainPage/MainPage.async"
+
 import "./index.scss"
 
 const App = () => {
   return (
     <div className="app">
-      aasdasdasd
-      <Counter />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
