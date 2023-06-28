86b4aa38099f88d095dc9653f9ffd2047033b673
monroo <andrey.monroo@gmail.com>
2023-06-28 20:22:34 +0300

    widget (navbar) appLink, themes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,7 @@
-import { Link } from "react-router-dom"
-
-import { classNames } from "shared/lib/classNames/classNames"
-
 import { useTheme } from "app/providers/ThemeProvider"
 import { AppRouter } from "app/providers/router"
+import { Navbar } from "widgets/Navbar"
+import { classNames } from "shared/lib/classNames/classNames"
 
 import "./index.scss"
 
@@ -12,9 +10,8 @@ const App = () => {
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to="/">Главная</Link>
-      <Link to="/about">О сайте</Link>
       <AppRouter />
     </div>
   )
