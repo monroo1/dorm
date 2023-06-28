721f9f5806b88f83209387de72f6f9a27140056b
monroo <andrey.monroo@gmail.com>
2023-06-28 19:06:07 +0300

    деструктуризация роутера

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,9 @@
-import { Route, Routes } from "react-router-dom"
 import { Link } from "react-router-dom"
-import { Suspense } from "react"
-
-import { AboutPage } from "pages/AboutPage"
-import { MainPage } from "pages/MainPage"
 
 import { classNames } from "shared/lib/classNames/classNames"
 
 import { useTheme } from "app/providers/ThemeProvider"
+import { AppRouter } from "app/providers/router"
 
 import "./index.scss"
 
@@ -19,12 +15,7 @@ const App = () => {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   )
 }
