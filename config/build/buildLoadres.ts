002be1501db0a9ad62e82da81b5389520f7f47d3
monroo <andrey.monroo@gmail.com>
2023-06-29 15:39:21 +0300

    add babel, extract plugin

--- config/build/buildLoadres.ts
+++ config/build/buildLoadres.ts
@@ -17,6 +17,23 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            { locales: ["ru", "en"], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  }
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -42,5 +59,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
-  return [svgLoader, fileLoader, typescriptLoader, cssLoader]
+  return [svgLoader, fileLoader, babelLoader, typescriptLoader, cssLoader]
 }
