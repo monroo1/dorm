e117e886ee36cfe979717103c195f7a56b3c63f5
monroo <andrey.monroo@gmail.com>
2023-07-04 20:58:43 +0300

    add config yaml githaub actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -21,11 +21,11 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
     if (isDev) {
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
         plugins.push(new ReactRefreshWebpackPlugin());
         plugins.push(new webpack.HotModuleReplacementPlugin());
     }
