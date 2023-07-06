8ce3860a5fd8a5ab0b812ecb41ae713641e3578d
monroo <andrey.monroo@gmail.com>
2023-07-06 19:02:08 +0300

    redux counter and tests

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "shared/ui/Button/Button";
+import { useTranslation } from "react-i18next";
+import { counterActions } from "../model/slice/counterSlice";
+import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue);
+    const { t } = useTranslation();
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+
+    return (
+        <div>
+            <h1 data-testid="value-title">
+
+                {counterValue}
+            </h1>
+            <Button
+                onClick={increment}
+                data-testid="increment-btn"
+            >
+                {t("Increment")}
+            </Button>
+            <Button
+                onClick={decrement}
+                data-testid="decrement-btn"
+            >
+                {t("Decrement")}
+            </Button>
+        </div>
+    );
+};
