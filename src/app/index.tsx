2fd7e1384c01a90e01b3026c5649c07c3ad4b8c9
monroo1 <andrey.monroo@gmail.com>
2023-07-13 11:37:16 +0300

    update auth only routes, fixs themes for modal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,14 @@ import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { classNames } from "shared/lib/classNames/classNames";
-import { useDispatch } from "react-redux";
-import { userActions } from "entities/User";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserInited, userActions } from "entities/User";
 import { useTheme } from "app/providers/ThemeProvider";
 
 function App() {
 	const { theme } = useTheme();
 	const dispatch = useDispatch();
+	const inited = useSelector(getUserInited);
 
 	useEffect(() => {
 		dispatch(userActions.initAuthData());
@@ -21,7 +22,7 @@ function App() {
 				<Navbar />
 				<div className="content-page">
 					<Sidebar />
-					<AppRouter />
+					{inited && <AppRouter />}
 				</div>
 			</Suspense>
 		</div>
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -5,6 +5,7 @@ import React, {
 import { Mods, classNames } from "shared/lib/classNames/classNames";
 import { Portal } from "shared/ui/Portal/Portal";
 
+import { useTheme } from "app/providers/ThemeProvider";
 import cls from "./Modal.module.scss";
 
 interface ModalProps {
@@ -25,6 +26,7 @@ export const Modal = (props: ModalProps) => {
 	const [isClosing, setIsClosing] = useState(false);
 	const [isMounted, setIsMounted] = useState(false);
 	const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
+	const { theme } = useTheme();
 
 	useEffect(() => {
 		if (isOpen) {
@@ -77,7 +79,7 @@ export const Modal = (props: ModalProps) => {
 
 	return (
 		<Portal>
-			<div className={classNames(cls.Modal, mods, [className])}>
+			<div className={classNames(cls.Modal, mods, [className, theme, "app_modal"])}>
 				<div className={cls.overlay} onClick={closeHandler}>
 					<div className={cls.content} onClick={onContentClick}>
 						{children}
