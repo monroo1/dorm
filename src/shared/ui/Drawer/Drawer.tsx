2b24030aecc703e7415a31bc7357f7e9ed82d8f9
monroo <andrey.monroo@gmail.com>
2023-08-09 17:40:57 +0300

    add drawer, device detector, create hook useModal

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,34 +1,30 @@
-import { ReactNode } from "react";
-import { Mods, classNames } from "shared/lib/classNames/classNames";
+import { ReactNode, memo } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
+import { Mods, classNames } from "shared/lib/classNames/classNames";
 import { useModal } from "shared/lib/hooks/useModal/useModal";
 import { Portal } from "../Portal/Portal";
-
-import cls from "./Modal.module.scss";
 import { Overlay } from "../Overlay/Overlay";
+import cls from "./Drawer.module.scss";
 
-interface ModalProps {
-	className?: string;
-	children?: ReactNode;
-	isOpen?: boolean;
-	onClose?: () => void;
-	lazy?: boolean;
+interface DrawerProps {
+    className?: string;
+    children: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+    lazy?: boolean;
 }
 
-const ANIMATION_DELAY = 300;
-
-export const Modal = (props: ModalProps) => {
+export const Drawer = memo((props: DrawerProps) => {
 	const {
-		className, children, isOpen, onClose, lazy,
+		className, children, isOpen, lazy, onClose,
 	} = props;
+	const { theme } = useTheme();
 
 	const { isClosing, isMounted, close } = useModal({
-		animationDelay: ANIMATION_DELAY,
+		animationDelay: 300,
 		onClose,
 		isOpen,
 	});
-	const { theme } = useTheme();
-
 	const mods: Mods = {
 		[cls.opened]: isOpen,
 		[cls.isClosing]: isClosing,
@@ -40,12 +36,13 @@ export const Modal = (props: ModalProps) => {
 
 	return (
 		<Portal>
-			<div className={classNames(cls.Modal, mods, [className, theme, "app_modal"])}>
+			<div className={classNames(cls.Drawer, mods, [className, theme, "app_drawer"])}>
 				<Overlay onClick={close} />
 				<div className={cls.content}>
 					{children}
 				</div>
+
 			</div>
 		</Portal>
 	);
-};
+});
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -46,6 +46,7 @@ export const Navbar = memo(({ className }: NavbarProps) => {
 					{t("создать статью")}
 				</AppLink>
 				<HStack gap="16" className={cls.actions}>
+
 					<NotificationButton />
 					<AvatarDropdown />
 				</HStack>
