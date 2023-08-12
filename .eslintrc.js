6d5565ce4c2c1243a550169141d32d510a22fa37
monroo <andrey.monroo@gmail.com>
2023-08-12 11:55:16 +0300

    add new version eslint plugin (supported alias)

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,7 +66,7 @@ module.exports = {
 		"no-param-reassign": "off",
 		"no-undef": "off",
 		"react/no-array-index-key": "off",
-		"monroo-plugin/path-checker": "error",
+		"monroo-plugin/path-checker": ["error", { alias: "@" }],
 	},
 	globals: {
 		__IS_DEV__: true,
