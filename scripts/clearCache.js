826064efd4785762c40edbc8645a5e94d253bcc7
monroo <andrey.monroo@gmail.com>
2023-08-22 12:42:35 +0300

    fix script for clean cache postinstall

--- scripts/clearCache.js
+++ scripts/clearCache.js
@@ -2,19 +2,24 @@ const path = require("path");
 const fs = require("fs");
 
 function clearCache() {
-	const pathCache = path.join(__dirname, "..", "node_modules", ".cache");
+	const pathCache = path.resolve(__dirname, "..", "node_modules", ".cache");
 
-	if (pathCache) {
-		fs.rm(
-			pathCache,
-			{ recursive: true },
-			(err) => {
-				if (err) {
-					throw err;
-				}
-			},
-		);
-	}
+	// console.log(pathCache);
+	fs.stat(pathCache, (err) => {
+		if (!err) {
+			fs.rm(
+				pathCache,
+				{ recursive: true },
+				(err) => {
+					if (err) {
+						throw err;
+					}
+				},
+			);
+		} else {
+			console.log("кэша нет!");
+		}
+	});
 }
 
 clearCache();
